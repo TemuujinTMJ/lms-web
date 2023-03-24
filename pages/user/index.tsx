8b08e7045a8adea74180739f30cee5895fb60735
Temuujin Munkhtugla <hades.tmj@gmail.com>
2023-03-24 19:42:40 +0800

    ss

--- pages/user/index.tsx
+++ pages/user/index.tsx
@@ -1,53 +1,57 @@
 import { Input, Select, Table, Tag } from "antd";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 import style from "./index.module.css";
 
-const dataSource = [
-  {
-    key: "1",
-    school: "Must-Sict",
-    floor: 3,
-    class: "310",
-    device: "Mac",
-    deviceCode: "123",
-    laborant: "Г.Мөнхбат",
-  },
-];
-
-const columns = [
-  {
-    title: "Сургууль",
-    dataIndex: "school",
-    key: "school",
-  },
-  {
-    title: "Давхар",
-    dataIndex: "floor",
-    key: "floor",
-  },
-  {
-    title: "Лабораторын ангийн дугаар",
-    dataIndex: "class",
-    key: "class",
-  },
-  {
-    title: "төхөөрөмжийн төрөл",
-    dataIndex: "device",
-    key: "device",
-  },
-  {
-    title: "төхөөрөмжийн код",
-    dataIndex: "deviceCode",
-    key: "deviceCode",
-  },
-  {
-    title: "лаборант хариуцагч",
-    dataIndex: "laborant",
-    key: "laborant",
-  },
-];
-
 const User = () => {
+  const router = useRouter();
+
+  const dataSource = [
+    {
+      key: "1",
+      school: <Link href={`${router.asPath}/mac`} >Must-Sicst</Link>,
+      floor: 3,
+      class: "310",
+      device: "Mac",
+      deviceCode: "123",
+      laborant: "Г.Мөнхбат",
+    },
+  ];
+
+  const columns = [
+    {
+      title: "Сургууль",
+      dataIndex: "school",
+      key: "school",
+    },
+    {
+      title: "Давхар",
+      dataIndex: "floor",
+      key: "floor",
+    },
+    {
+      title: "Лабораторын ангийн дугаар",
+      dataIndex: "class",
+      key: "class",
+    },
+    {
+      title: "төхөөрөмжийн төрөл",
+      dataIndex: "device",
+      key: "device",
+    },
+    {
+      title: "төхөөрөмжийн код",
+      dataIndex: "deviceCode",
+      key: "deviceCode",
+    },
+    {
+      title: "лаборант хариуцагч",
+      dataIndex: "laborant",
+      key: "laborant",
+    },
+  ];
+
   return (
     <div>
       <div className={style.header}>
