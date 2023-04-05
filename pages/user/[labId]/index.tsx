a44ec240cb189f076203d65e83f1ce61726d2e60
Temuujin Munkhtugla <hades.tmj@gmail.com>
2023-04-06 01:54:04 +0800

    sdsd

--- pages/user/[labId]/index.tsx
+++ pages/user/[labId]/index.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import { useEffect, useState } from 'react';
 import { Calendar, DateRange } from 'react-date-range';
 import ReactPannellum from 'react-pannellum';
-import { Button, Card, Carousel, Checkbox, Col, Image, Input, Modal, Popover, Radio, Row, message } from 'antd';
+import { Button, Card, Carousel, Checkbox, Col, Image, Input, message, Modal, Popover, Radio, Row } from 'antd';
 import { CheckboxValueType } from 'antd/es/checkbox/Group';
 import dayjs from 'dayjs';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
@@ -68,6 +68,7 @@ const Index = () => {
     dispatch(postUserDevoceOrders({ device_id: dev?._id, date: isoString, type: 'hour' }));
     setIsModalOpen(true);
   };
+  const [contextHolder] = message.useMessage();
   const handleOk = () => {
     setIsModalOpen(false);
     if (value === 'day') {
@@ -105,6 +106,7 @@ const Index = () => {
   };
   const handleCancel = () => {
     setIsModalOpen(false);
+    console.log('first');
   };
   const [uniqueDates, setUniqueDates] = useState<Date[]>([]);
   useEffect(() => {
@@ -151,196 +153,199 @@ const Index = () => {
   return submitLoading ? (
     <></>
   ) : (
-    <div style={{ padding: '20px' }}>
-      <Row>
-        <Col className={style.cols} span={8}>
-          <div className={style.col}>
-            <div className={style.text}>Сургууль</div>
-            <div className={style.card}>{laboratory?.school}</div>
-          </div>
-        </Col>
-        <Col className={style.cols} span={8}>
-          <div className={style.col}>
-            <div className={style.text}>Лабораторийн ангийн дугаар</div>
-            <div className={style.card}>{laboratory?.room}</div>
-          </div>
-        </Col>
-        <Col className={style.cols} span={8}>
-          <div className={style.col}>
-            <div className={style.text}>Давхар</div>
-            <div className={style.card}>{laboratory?.floor}</div>
-          </div>
-        </Col>
-        <Col className={style.cols} span={8}>
-          <div className={style.col}>
-            <div className={style.text}>Лабораторийн нэршил</div>
-            <div className={style.card}>{laboratory?.title}</div>
-          </div>
-        </Col>
-        <Col className={style.cols} span={8}>
-          <div className={style.col}>
-            <div className={style.text}>Хариуцсан багшийн нэр</div>
-            <div className={style.card}>
-              <Popover
-                content={
-                  <>
-                    <div className={style.flex}>
-                      <div>Овог:</div>
-                      <div>{laboratory?.teacher?.last_name}</div>
-                    </div>
-                    <div className={style.flex}>
-                      <div>Нэр:</div>
-                      <div>{laboratory?.teacher?.first_name}</div>
-                    </div>
-                    <div className={style.flex}>
-                      <div>Утас:</div>
-                      <div>{laboratory?.teacher?.phone}</div>
-                    </div>
-                    <div className={style.flex}>
-                      <div>email:</div>
-                      <div>{laboratory?.teacher?.email}</div>
-                    </div>
-                  </>
-                }
-                title="Багшийн мэдээлэл"
-                trigger="click"
-              >
-                <a>{laboratory?.teacher?.first_name}</a>
-              </Popover>
+    <>
+      {contextHolder}
+      <div style={{ padding: '20px' }}>
+        <Row>
+          <Col className={style.cols} span={8}>
+            <div className={style.col}>
+              <div className={style.text}>Сургууль</div>
+              <div className={style.card}>{laboratory?.school}</div>
             </div>
-          </div>
-        </Col>
-      </Row>
-      <Row>
-        <Col span={12} className={style.cols}>
-          <Carousel autoplay style={{ width: '400px' }}>
+          </Col>
+          <Col className={style.cols} span={8}>
+            <div className={style.col}>
+              <div className={style.text}>Лабораторийн ангийн дугаар</div>
+              <div className={style.card}>{laboratory?.room}</div>
+            </div>
+          </Col>
+          <Col className={style.cols} span={8}>
+            <div className={style.col}>
+              <div className={style.text}>Давхар</div>
+              <div className={style.card}>{laboratory?.floor}</div>
+            </div>
+          </Col>
+          <Col className={style.cols} span={8}>
+            <div className={style.col}>
+              <div className={style.text}>Лабораторийн нэршил</div>
+              <div className={style.card}>{laboratory?.title}</div>
+            </div>
+          </Col>
+          <Col className={style.cols} span={8}>
+            <div className={style.col}>
+              <div className={style.text}>Хариуцсан багшийн нэр</div>
+              <div className={style.card}>
+                <Popover
+                  content={
+                    <>
+                      <div className={style.flex}>
+                        <div>Овог:</div>
+                        <div>{laboratory?.teacher?.last_name}</div>
+                      </div>
+                      <div className={style.flex}>
+                        <div>Нэр:</div>
+                        <div>{laboratory?.teacher?.first_name}</div>
+                      </div>
+                      <div className={style.flex}>
+                        <div>Утас:</div>
+                        <div>{laboratory?.teacher?.phone}</div>
+                      </div>
+                      <div className={style.flex}>
+                        <div>email:</div>
+                        <div>{laboratory?.teacher?.email}</div>
+                      </div>
+                    </>
+                  }
+                  title="Багшийн мэдээлэл"
+                  trigger="click"
+                >
+                  <a>{laboratory?.teacher?.first_name}</a>
+                </Popover>
+              </div>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={12} className={style.cols}>
+            <Carousel autoplay style={{ width: '400px' }}>
+              {laboratory?.medias.map((e, key) => {
+                return e?.type === 'image' ? (
+                  <Image key={key} alt="" width={400} src={`${config.HOST}${e?.path}`} />
+                ) : null;
+              })}
+            </Carousel>
+          </Col>
+          <Col span={12}>
             {laboratory?.medias.map((e, key) => {
-              return e?.type === 'image' ? (
-                <Image key={key} alt="" width={400} src={`${config.HOST}${e?.path}`} />
+              return e?.type === 'pano' ? (
+                <ReactPannellum key={key} id="1" sceneId="firstScene" imageSource={`${config.HOST}${e?.path}`} />
               ) : null;
             })}
-          </Carousel>
-        </Col>
-        <Col span={12}>
-          {laboratory?.medias.map((e, key) => {
-            return e?.type === 'pano' ? (
-              <ReactPannellum key={key} id="1" sceneId="firstScene" imageSource={`${config.HOST}${e?.path}`} />
-            ) : null;
+          </Col>
+        </Row>
+        <h1 style={{ marginLeft: '70px' }}>Төхөөрөмжүүд</h1>
+        <Row wrap style={{ margin: '0px 10px 50px 10px' }}>
+          {laboratory?.devices?.map((dev, key) => {
+            return (
+              <Col span={12} className={style.cols} key={key}>
+                <Card hoverable title={dev?.title} extra={<Button onClick={() => showModal(dev)}>Захиалах</Button>}>
+                  <div className={style.device}>
+                    <Image key={key} alt="" width={200} height={200} src={`${config.HOST}${dev?.medias[0]?.path}`} />
+                    <ul style={{ margin: '0px', width: '300px' }}>
+                      <li className={style.list}>
+                        <span style={{ fontWeight: '700' }}>- Төхөөрөмж:</span> {dev?.title}
+                      </li>
+                      <li className={style.list}>
+                        <span style={{ fontWeight: '700' }}>- Модел:</span> {dev?.model}
+                      </li>
+                      <li className={style.list}>
+                        <span style={{ fontWeight: '700' }}>- Үйлдвэрлэсэн он:</span>{' '}
+                        {moment(dev?.manufacturedDate).format('YYYY-MM-DD')}
+                      </li>
+                      <li className={style.list}>
+                        <span style={{ fontWeight: '700' }}>- Төхөөрөмжийн үнэлгээ:</span> {dev?.price} MNT
+                      </li>
+                    </ul>
+                  </div>
+                </Card>
+              </Col>
+            );
           })}
-        </Col>
-      </Row>
-      <h1 style={{ marginLeft: '70px' }}>Төхөөрөмжүүд</h1>
-      <Row wrap style={{ margin: '0px 10px 50px 10px' }}>
-        {laboratory?.devices?.map((dev, key) => {
-          return (
-            <Col span={12} className={style.cols} key={key}>
-              <Card hoverable title={dev?.title} extra={<Button onClick={() => showModal(dev)}>Захиалах</Button>}>
-                <div className={style.device}>
-                  <Image key={key} alt="" width={200} height={200} src={`${config.HOST}${dev?.medias[0]?.path}`} />
-                  <ul style={{ margin: '0px', width: '300px' }}>
-                    <li className={style.list}>
-                      <span style={{ fontWeight: '700' }}>- Төхөөрөмж:</span> {dev?.title}
-                    </li>
-                    <li className={style.list}>
-                      <span style={{ fontWeight: '700' }}>- Модел:</span> {dev?.model}
-                    </li>
-                    <li className={style.list}>
-                      <span style={{ fontWeight: '700' }}>- Үйлдвэрлэсэн он:</span>{' '}
-                      {moment(dev?.manufacturedDate).format('YYYY-MM-DD')}
-                    </li>
-                    <li className={style.list}>
-                      <span style={{ fontWeight: '700' }}>- Төхөөрөмжийн үнэлгээ:</span> {dev?.price} MNT
-                    </li>
-                  </ul>
-                </div>
-              </Card>
-            </Col>
-          );
-        })}
-      </Row>
-      <Modal
-        title={device?.title}
-        open={isModalOpen}
-        onOk={handleOk}
-        onCancel={handleCancel}
-        width={1000}
-        okText="Захиалах"
-        cancelText="Цуцлах"
-      >
-        <div>
-          <Radio.Group defaultValue="hour" size="large" onChange={(e) => orderFunc(e?.target?.value)}>
-            <Radio.Button value="hour">Цагаар</Radio.Button>
-            <Radio.Button value="day">Өдрөөр</Radio.Button>
-          </Radio.Group>
-          <Row style={{ marginTop: '20px', marginBottom: '50px' }}>
-            {value === 'day' ? (
-              <>
-                <Col span={9}>
-                  <DateRange
-                    minDate={new Date()}
-                    editableDateInputs={true}
-                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                    onChange={(item: any) => setState([item?.selection])}
-                    moveRangeOnFirstSelection={false}
-                    ranges={state}
-                    disabledDates={uniqueDates}
-                  />
-                </Col>
-                <Col span={15}>
-                  <div style={{ fontSize: '16px', fontWeight: 'bold', border: '1px solid #eeeeee' }}>
-                    Ашиглах зорилго:
-                  </div>
-                  <TextArea
-                    style={{ border: 'none', height: '300px', overflow: 'auto' }}
-                    value={desc}
-                    onChange={(e) => setDesc(e?.target?.value)}
-                  />
-                </Col>
-              </>
-            ) : (
-              <>
-                <Col span={9}>
-                  <div
-                    style={{
-                      border: '1px solid #eeeeee',
-                      paddingBottom: '10px',
-                    }}
-                  >
-                    <Calendar onChange={(item) => onDate(item)} date={date} minDate={new Date()} />
-                  </div>
-                </Col>
-                <Col span={8}>
-                  <Checkbox.Group onChange={onChange} value={checkedList}>
-                    <Row wrap>
-                      {hours.map((e, key) => {
-                        return (
-                          <Col key={key} span={8} className={style.orderCard}>
-                            <div>{e?.hour}</div>
-                            <Checkbox value={e?.value} disabled={orders.includes(e?.value)}>
-                              {orders.includes(e?.value) ? 'Захиалсан' : 'Захиалах'}
-                            </Checkbox>
-                          </Col>
-                        );
-                      })}
-                    </Row>
-                  </Checkbox.Group>
-                </Col>
-                <Col span={7}>
-                  <div style={{ fontSize: '16px', fontWeight: 'bold', border: '1px solid #eeeeee' }}>
-                    Ашиглах зорилго:
-                  </div>
-                  <TextArea
-                    style={{ border: 'none', height: '300px', overflow: 'auto' }}
-                    value={desc}
-                    onChange={(e) => setDesc(e?.target?.value)}
-                  />
-                </Col>
-              </>
-            )}
-          </Row>
-        </div>
-      </Modal>
-    </div>
+        </Row>
+        <Modal
+          title={device?.title}
+          open={isModalOpen}
+          onOk={handleOk}
+          onCancel={handleCancel}
+          width={1000}
+          okText="Захиалах"
+          cancelText="Цуцлах"
+        >
+          <div>
+            <Radio.Group defaultValue="hour" size="large" onChange={(e) => orderFunc(e?.target?.value)}>
+              <Radio.Button value="hour">Цагаар</Radio.Button>
+              <Radio.Button value="day">Өдрөөр</Radio.Button>
+            </Radio.Group>
+            <Row style={{ marginTop: '20px', marginBottom: '50px' }}>
+              {value === 'day' ? (
+                <>
+                  <Col span={9}>
+                    <DateRange
+                      minDate={new Date()}
+                      editableDateInputs={true}
+                      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                      onChange={(item: any) => setState([item?.selection])}
+                      moveRangeOnFirstSelection={false}
+                      ranges={state}
+                      disabledDates={uniqueDates}
+                    />
+                  </Col>
+                  <Col span={15}>
+                    <div style={{ fontSize: '16px', fontWeight: 'bold', border: '1px solid #eeeeee' }}>
+                      Ашиглах зорилго:
+                    </div>
+                    <TextArea
+                      style={{ border: 'none', height: '300px', overflow: 'auto' }}
+                      value={desc}
+                      onChange={(e) => setDesc(e?.target?.value)}
+                    />
+                  </Col>
+                </>
+              ) : (
+                <>
+                  <Col span={9}>
+                    <div
+                      style={{
+                        border: '1px solid #eeeeee',
+                        paddingBottom: '10px',
+                      }}
+                    >
+                      <Calendar onChange={(item) => onDate(item)} date={date} minDate={new Date()} />
+                    </div>
+                  </Col>
+                  <Col span={8}>
+                    <Checkbox.Group onChange={onChange} value={checkedList}>
+                      <Row wrap>
+                        {hours.map((e, key) => {
+                          return (
+                            <Col key={key} span={8} className={style.orderCard}>
+                              <div>{e?.hour}</div>
+                              <Checkbox value={e?.value} disabled={orders.includes(e?.value)}>
+                                {orders.includes(e?.value) ? 'Захиалсан' : 'Захиалах'}
+                              </Checkbox>
+                            </Col>
+                          );
+                        })}
+                      </Row>
+                    </Checkbox.Group>
+                  </Col>
+                  <Col span={7}>
+                    <div style={{ fontSize: '16px', fontWeight: 'bold', border: '1px solid #eeeeee' }}>
+                      Ашиглах зорилго:
+                    </div>
+                    <TextArea
+                      style={{ border: 'none', height: '300px', overflow: 'auto' }}
+                      value={desc}
+                      onChange={(e) => setDesc(e?.target?.value)}
+                    />
+                  </Col>
+                </>
+              )}
+            </Row>
+          </div>
+        </Modal>
+      </div>
+    </>
   );
 };
 
