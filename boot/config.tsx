f8ac4eba5bd0ed7959f9e6aa1b1d37ebe43570de
Temuujin Munkhtugla <hades.tmj@gmail.com>
2023-12-13 16:32:13 +0800

    dom

--- boot/config.tsx
+++ boot/config.tsx
@@ -1,5 +1,4 @@
 export const config = {
-  // HOST: 'http://lims.sict.edu.mn/api',
+  HOST: 'http://lims.sict.edu.mn/api',
   // HOST: 'http://localhost:3001',
-  HOST: 'http://202.70.34.22:3001',
 };
